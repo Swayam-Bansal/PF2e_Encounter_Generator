4fa9f6b4ea6d3a8b42556e8d09ebb1dd5defc44f
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-07-26 12:29:18 -0400

    Fixed output directory for JSON files

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # DB_NAME = monsters.db
 MONSTER_JSON_PATH = 'beastiary/monsters.json'
-download_directory = 'json_files'
+download_directory = 'beastiary'
 raw_json_base = "https://raw.githubusercontent.com/Pf2eToolsOrg/Pf2eTools/dev/data/bestiary/"
 api_url = "https://api.github.com/repos/Pf2eToolsOrg/Pf2eTools/contents/data/bestiary"
